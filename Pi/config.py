62846565091ef41236536787e90618523ce05691
JefftheGreen <jeff.andrew.collins@gmail.com>
2015-06-24 04:00:48 +0000

    Modified main file architecture:  *accepts options (no camera, no motion, echo, static config, used pins, and help)  *broken into functions for possible future threading to connect to multiple UIs  *now accepts data connection and then connects to UI to deliver camera (see Pi/Server/ssl_protocol.txt)  *adds servos and actuators  *tries a number of data ports in case one or more is in use  *cleans up if a thread is aborted or an exception is raised in a thread  *echo, no motion, and no camera are session variables stored in config, along with static config and used pins

--- Pi/config.py
+++ Pi/config.py
@@ -2,31 +2,61 @@
 # -*- coding: utf-8 -*-
 
 import tempfile
+import os
 
 #SSL
-__camera_port =     [8080, 8081, 8082, 8083]
-__data_port =       [8090, 8091, 8092, 8093]
-__cert_file =       'cert.pem'
-__key_file =        'key.pem'
+__data_ports =          [8090, 8091, 8092, 8093]
+__cert_file =           'cert.pem'
+__key_file =            'key.pem'
 
-#Doesn't matter
+#IO
+__altitude_pin =        1
+__altitude_rangev =     90
+__altitude_pulse =      [1.0, 2.0]
+__altitude_reverse =    False
+__azimuth_pin =         2
+__azimuth_rangev =      90
+__azimuth_pulse =       [1.0, 2.0]
+__azimuth_reverse =     False
+__trigger_pin =         3
+__trigger_type =        'pull'
+__safety_pin =          4
+__safety_type =         'pull'
+__safety_on_position =  'out'
+
+#Formulas -- do not change
+self_path = os.path.realpath(__file__)
+path = self_path[:(-1*len(__file__))]
+
+#Sesion Variables -- initialized in script
 echo = None
-
-#################################################################
+used_pins = None
+static = None
+motion_on = None
+camera_on = None
 
 def rewrite():
-    with tempfile.TemporaryFile(mode='r+') as tmp:
-        with open('Pi/config.py') as configfile:
-            for line in configfile:
-                if '__' in line and not 'elif' in line:
-                    tmp.write((line[0:21]))
-                    print(tmp.read())
-                    variable = line.split()[0]
-                    tmp.write(str(eval(variable)))
-                    tmp.write('\n')
+    #Open config script, closing even with an exception.
+    with open(self_path) as configfile:
+        #Create empty file string
+        tmp = ''
+        for line in configfile:
+            #Variable lines
+            if line[0:2] == '__':
+                #Add everything but the value
+                tmp += (line[0:24])
+                variable = line.split()[0]
+                #Add the variable
+                if isinstance(eval(variable), str):
+                    variable = "'" + str(eval(variable)) + "'"
                 else:
-                    tmp.write(line)
-#        with open('Pi/config.py', 'wt+') as configfile:
-#            for line in tmp:
-#                print(line)
-#                configfile.write(line)
+                    variable = str(eval(variable))
+                tmp += variable
+                tmp += '\n'
+            #Non-variable lines
+            else:
+                #Add line as-is
+                tmp += line
+    #Open config script, write over it, closing even with an exception.
+    with open(self_path, 'w+') as configfile:
+        configfile.write(tmp)
--- Pi/main.py
+++ Pi/main.py
@@ -12,7 +12,9 @@
 #                               terminal.
 #       -m, --no-motion         Removes motion function, replacing it with a
 #                               dummy capability to provide necesaary variables.
-#
+#       -C, --static-config     The config file is not saved.
+#       -p, --used-pins         Pins that are not to be used, separated by 
+#                               semicolons.
 #       -h, -?, --help          Displays help file.
 
 import Server.piserver as piserver
@@ -29,49 +31,160 @@ import getopt
 import config
 import sys
 
-
-options, arguments = getopt.getopt(sys.argv[1:],
-                                   'cmeh?', 
-                                   ['no-camera', 
-                                    'no-motion',
-                                    'echo',
-                                    'help'])
-
-if '-h' in options or '--help' in options or '?' in options:
-    helpfile = open('help.txt', 'w')
-    print (helpfile.read)
-    sys.exit()
-
-motion = False if '-m' in options or '--no-motion' in options else True
-camera = False if '-c' in options or '--no-camera' in options else True
-echo = True if '-e' in options or '--echo' in options else False
-config.echo = echo
-
-camera_socket = ssl.wrap_socket(socket.socket(socket.AF_INET, 
-                                              socket.SOCK_STREAM), 
-                                server_side = True, 
-                                certfile = config.cert_file, 
-                                keyfile = config.key_file)
-data_socket = ssl.wrap_socket(socket.socket(socket.AF_INET, 
-                                            socket.SOCK_STREAM), 
-                              server_side = True, 
-                              certfile = config.cert_file, 
-                              keyfile = config.key_file)
-                              
-camera_socket.bind('', config.camera_port)
-data_socket.bind('', config.camera_port)
-camera_socket.listen(1)
-data_socket.listen(1)
-with camera_controller.Camera_Constructor(camera_socket) as camera:
-    motion = motion_controller.Motion_Controller(camera)
-    camera_queue = queue.Queue()
-    control_queue = queue.Queue()
-    server_queue = queue.Queue()
-    camera_thread = piserver.Camera_Process(camera, camera_queue, control_queue)
-    server_thread = piserver.Server_Process(data_socket, 
-                                            server_queue, 
-                                            control_queue)
-    control_thread = control.Control_Process(camera_queue, motion_controller, control_queue, server_queue)
-    camera_thread.start()
-    server_thread.start()
-    control_thread.start()
+#Connects to a UI and executes commands received from it. 
+#*Follows the protocol in Server/ssl_protocol.txt for connection.
+#*Creates objects to control motion and camera.
+#*Launches threads for camera, server, and control
+#*Waits for a thread to terminate, then calls all threads to terminate.
+#*Cleans up.
+def connect_and_run():
+    try:
+        #Create data connection and wait for connection
+        print('Connecting data')
+        port_number = 0
+        tried_ports = []
+        #Create socket at open port, raise OSError if none are available
+        while True:
+            try:
+                data_socket = ssl.wrap_socket(socket.socket(socket.AF_INET, 
+                                                            socket.SOCK_STREAM), 
+                                              server_side = True, 
+                                              certfile = config.cert_file, 
+                                              keyfile = config.key_file)
+                data_socket.bind('', config.data_ports[port_number])
+                data_socket.listen(1)
+                data_client, address = data_socket.accept()
+            except OSError:
+                if port_number < len(config.__data_ports) - 1:
+                    port_number += 1
+                else:
+                    raise
+            
+        #Wait for Port from UI
+        while True:
+            port = int.from_bytes(data_client.recv(65536))
+            if port:
+                if port not in tried_ports:
+                    break
+        #Connect to UI with camera connection
+        if config.camera_on:
+            if config.echo:
+                print('Connecting camera')
+            camera_socket = ssl.wrap_socket(socket.socket(socket.AF_INET, 
+                                                          socket.SOCK_STREAM), 
+                                            server_side = False, 
+                                            certfile = config.cert_file, 
+                                            keyfile = config.key_file)                              
+            camera_socket.connect((address, port))
+        else:
+            camera_socket = None
+            #Context manager for camera_controller
+        with camera_controller.Camera_Constructor(camera_socket) as camera:
+            #Create motion controller
+            if config.motion_on:
+                motion = motion_controller.Motion_Controller(camera)
+            else:
+                motion = motion_controller.Dummy_Motion_Controller()
+            #Add servos and actuators to motion controller
+            motion.add_servo('altitude', 
+                             config.__altitude_pin, 
+                             config.__altitude_range,
+                             config.__altitude_pulse,
+                             config.__altitude_reverse)
+            motion.add_servo('azimuth', 
+                             config.__azimuth_pin, 
+                             config.__azimuth_range,
+                             config.__azimuth_pulse,
+                             config.__azimuth_reverse)
+            motion.add_actuator('trigger',
+                                config.__trigger_pin,
+                                config.__trigger_type)
+            motion.add_actuator('safety',
+                                config.__safety_pin,
+                                config.__safety_type)
+            #Create threads
+            camera_queue = queue.Queue()
+            control_queue = queue.Queue()
+            server_queue = queue.Queue()
+            stop_flag = threading.Event()
+            threads = []
+            camera_thread = piserver.Camera_Process(camera, 
+                                                    camera_queue, 
+                                                    control_queue,
+                                                    stop_flag)
+            server_thread = piserver.Server_Process(data_socket, 
+                                                    server_queue, 
+                                                    control_queue,
+                                                    stop_flag)
+            control_thread = control.Control_Process(camera_queue, 
+                                                     motion, 
+                                                     control_queue, 
+                                                     server_queue,
+                                                     stop_flag)
+            #Start threads  
+            if config.echo:
+                print('Starting camera thread')
+            camera_thread.start()
+            threads.append(camera_thread)
+            if config.echo:
+                print('Starting server thread')
+            server_thread.start()
+            threads.append(server_thread)
+            if config.echo:
+                print('Starting control thread')
+            control_thread.start()
+            threads.append(control_thread)
+            while True:
+                alive = [i.is_alive() for i in threads]
+                if not all(alive):
+                    stop_flag.set()
+                    break
+            
+    except:
+        data_socket.close()
+        data_client.close()
+        camera_socket.close()
+        raise
+        
+#Handles options, including setting session variables in config
+def handle_options():
+    #Get and process options
+    options, arguments = getopt.getopt(sys.argv[1:],
+                                       'cCmeph?', 
+                                       ['no-camera', 
+                                        'no-motion',
+                                        'echo',
+                                        'help',
+                                        'static-config',
+                                        'used-pins'])
+    for opt, arg in options:
+        #Display help file
+        if opt in ['-h', '--help', '-?']:
+            helpfile = open('help.txt', 'w')
+            print (helpfile.read)
+            sys.exit()
+        #Turn on or off echoing, motion control, and camera control according to
+        #options given at command line.
+        config.echo = True if opt in ['-e', '--e'] else False
+        config.motion_on = False if opt in ['-m', '--no-motion'] else True
+        if config.echo:
+            print('Motion {0}'.format(('enabled' if config.motion_on 
+                                       else 'disabled')))
+        config.camera_on = False if opt in ['-c', '--no-camera'] else True
+        if config.echo:
+            print('Camera {0}'.format(('enabled' if config.camera_on 
+                                       else 'disabled')))
+        #Disable rewriting the config file
+        config.static = True if opt in ['-C', '--static-config'] else False
+        #Reserve pins
+        if opt in ['-p', '--used-pins']:
+            config.used_pins = arg.replace(';' ' ').split()
+        else:
+            config.used_pins = []
+            
+def main():
+    #Connect to a UI and accept its commands
+    connect_and_run()
+    #Rewrite config
+    if not config.static():
+        config.rewrite()
