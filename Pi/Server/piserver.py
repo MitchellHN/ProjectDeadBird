673fb1e32341ee97d5d864a5ab2bd9f01f307fe2
JefftheGreen <jeff.andrew.collins@gmail.com>
2015-06-24 04:09:50 +0000

    finished first draft of server threads

--- Pi/Server/piserver.py
+++ Pi/Server/piserver.py
@@ -6,6 +6,7 @@ import ssl
 import multiprocessing
 import threading
 import queue
+import config
 
 class Camera_Process (threading.Thread):
     			
@@ -62,15 +63,21 @@ class Camera_Process (threading.Thread):
 
 class Server_Process (threading.Thread):
 	
-	def __init__(self, socket, read_queue, control_queue):
+	def __init__(self, socket, read_queue, control_queue, stop_flag):
 		self.socket = socket
 		self.read_queue = read_queue
 		self.control_queue = control_queue
 		self.send_queue = queue.Queue()
 		self.receive_queue = queue.Queue()
-		self.send_thread = Send_Thread(socket, self.send_queue)
-		self.receive_thread = Receive_Thread(socket, self.receive_queue)
-		self.commands = {''''0x00': self.receive_error,
+		self.stop_flag = stop_flag
+		self.child_stop_flag = threading.Event()
+		self.send_thread = Send_Thread(socket, 
+									   self.send_queue, 
+									   self.child_stop_flag)
+		self.receive_thread = Receive_Thread(socket, 
+											 self.receive_queue, 
+											 self.child_stop_flag)
+		self.commands = {'0x0': self.receive_error,
 						 '0x10': self.move_to_pixel,
 						 '0x11': self.move_to_altitude,
 						 '0x12': self.change_altitude,
@@ -85,12 +92,22 @@ class Server_Process (threading.Thread):
 						 '0x51': self.get_altitude,
 						 '0x52': self.get_azimuth,
 						 '0x53': self.get_safety,
-						 '0x54': self.get_locations'''}
+						 '0x54': self.get_locations}
+		self.errors = {
+				  }
 		
 	def run(self):
+		threads = []
 		self.send_thread.start()
 		self.receive_thread.start()
 		while True:
+			if self.stop_flag.is_set():
+				self.child_stop_flag.set()
+				break
+			alive = [i.is_alive() for i in threads]
+			if self.child_stop_flag.is_set() or not all(alive):
+				self.child_stop_flag.set()
+				self.stop_flag.set()
 			if not self.read_queue.empty():
 				self.interpret_read()
 			if not self.receive_queue.empty():
@@ -99,25 +116,8 @@ class Server_Process (threading.Thread):
 	def interpret_read(self):
 		#Get command from queue
 		q = self.read_queue.get()
-		#Extract command and arguments (if any)
-		if isinstance(q, list):
-			command = q.pop(0)
-			arguments = q
-		elif isinstance(q, str):
-			command = q
-			arguments = []
-		else:
-			raise TypeError('Something other than list or str was ' + 
-							 'added to the camera queue.')
-		#Try to execute command, raise errors if necessary
-		try:
-			self.commands[command](*arguments)
-		except KeyError:
-			print("%r is not an acceptable camera command" % command)
-		except TypeError:
-			message = "The %r command was given the arguments %r" % (command, arguments)
-			print(message)
-			raise
+		#Feed command to send thread
+		self.send_queue.push(q)
 		
 	def interpret_receive(self):
 		#Get command from queue
@@ -126,9 +126,6 @@ class Server_Process (threading.Thread):
 		if isinstance(q, list):
 			command = q.pop(0)
 			arguments = q
-		elif isinstance(q, str):
-			command = q
-			arguments = []
 		else:
 			raise TypeError('Something other than list or str was ' + 
 							 'added to the camera queue.')
@@ -136,21 +133,131 @@ class Server_Process (threading.Thread):
 		try:
 			self.commands[command](*arguments)
 		except KeyError:
-			print("%r is not an acceptable camera command" % command)
+			print("%r is not an acceptable byte command." % command)
 		except TypeError:
-			message = "The %r command was given the arguments %r" % (command, arguments)
+			message = "The %r command was given the arguments %r." % (command, 
+																	  arguments)
 			print(message)
 			raise
+		
+	def receive_error(self, error):
+		#get hex string
+		error = hex(int.from_bytes(error))
+		self.control_queue.push(['error', self.errors[error]])
+	
+	def move_to_pixel(self, pixel):
+		pixel_number = int.from_bytes(pixel, byteorder='big')
+		if config.echo:
+			print('Received command to move to pixel %s.' % pixel_number)
+		self.control_queue.push(['move_to_pixel', pixel_number])
+		
+	def move_to_altitude(self, altitude):
+		altitude = int.from_bytes(altitude, byteorder = 'big')
+		altitude /= 100.0
+		if config.echo:
+			print('Received command to move to altitude %s.' % altitude)
+		self.control_queue.push(['move to altitude', altitude])
+		
+	def change_altitude(self, change):
+		change = int.from_bytes(change, byteorder = 'big', signed = True)
+		change /= 100.0
+		if config.echo:
+			print('Received command to change altitude by %s.' % change)
+		self.control_queue.push(['change altitude', change])
+		
+	def move_to_azimuth(self, azimuth):
+		azimuth = int.from_bytes(azimuth, byteorder = 'big')
+		azimuth /= 100.0
+		if config.echo:
+			print('Received command to move to azimuth %s.' % azimuth)
+		self.control_queue.push(['move to azimuth', azimuth])
+		
+	def change_azimuth(self, change):
+		change = int.from_bytes(change, byteorder = 'big', signed = True)
+		change /= 100.0
+		if config.echo:
+			print('Received command to change azimuth by %s.' % azimuth)
+		self.control_queue.push(['change azimuth', change])
+		
+	def move_to_location(self, location):
+		if config.echo:
+			print('Received command to move to location %s.' % 
+				  int.from_bytes(location, 'big'))
+		self.control_queue.push(['move to location', 
+								 int.from_bytes(location, 'big')])
+		
+	def set_safety(self, on_off):
+		#If the first bit is 1, i.e. >= i.e. 1000 0000 0000 0000 0000 0000 in 
+		#binary, turn the safety on
+		if int.from_bytes(on_off, 'big') >= 8388608:
+			position = 'on'
+		else:
+			position = 'off'
+		if config.echo:
+			print('Received command to set safety %s.' % position)
+		self.control_queue.push(['set safety', position])
+		
+	def fire(self, irrelevant):
+		if config.echo:
+			print('Received command to fire.')
+		self.control_queue.push('fire')
+	
+	def move_reticle(self, pixel):
+		pixel_number = int.from_bytes(pixel, byteorder='big')
+		if config.echo:
+			print('Received command to set reticle to pixel %s.' % pixel_number)
+		self.control_queue.push(['move reticle', pixel_number])
+		
+	def store_location(self, irrelevant):
+		if config.echo:
+			print('Received command to set reticle to store location.')
+		self.control_queue.push('store location')
+		
+	def get_reticle(self, irrelevant):
+		if config.echo:
+			print('Received command to get reticle position.')
+		self.control_queue.push('send reticle')
+		
+	def get_altitude(self, irrelevant):
+		if config.echo:
+			print('Received command to get reticle position.')
+		self.control_queue.push('send altitude')
+		
+	def get_azimuth(self, irrelevant):
+		if config.echo:
+			print('Received command to get reticle position.')
+		self.control_queue.push('send azimuth')
+		
+	def get_safety(self, irrelevant):
+		if config.echo:
+			print('Received command to get reticle position.')
+		self.control_queue.push('send azimuth')
+		
+	def get_locations(self, irrelevant):
+		if config.echo:
+			print('Received command to get reticle position.')
+		self.control_queue.push('send azimuth')	
+		
+		
+	
 				
 class Send_Thread (threading.Thread):
 	
-	def __init__(self, socket, read_queue):
-		self.socket = socket
+	def __init__(self, client, read_queue, stop_flag):
+		self.client = client
 		self.read_queue = read_queue
-		self.commands = {}
+		self.stop_flag = stop_flag
+		self.commands = {'send_error': self.send_error,
+						 'send reticle': self.send_reticle,
+						 'send altitude': self.send_altitude,
+						 'send azimuth': self.send_azimuth,
+						 'send safety': self.send_safety,
+						 'send locations': self.send_locations}
 		
 	def run(self):
 		while True:
+			if self.stop_flag.is_set():
+				break
 			if not self.read_queue.empty():
 				#Get command from queue
 				q = self.read_queue.get()
@@ -170,21 +277,54 @@ class Send_Thread (threading.Thread):
 				except KeyError:
 					print("%r is not an acceptable camera command" % command)
 				except TypeError:
-					message = "The %r command was given the arguments %r" % (command, arguments)
+					message = ("The %r command was given the arguments %r" % 
+							   (command, arguments))
 					print(message)
 					raise
 		
+	def send_error(self, error_ID):
+		self.send(0, error_ID)
+				
+	def send_reticle(self, pixel_number):
+		self.send(1, pixel_number)
+			
+	def send_altitude(self, altitude):
+		self.send(2, altitude * 100)
+		
+	def send_azimuth(self, azimuth):
+		self.send(2, azimuth * 100)
+		
+	def send_safety(self, safety):
+		argument = bytes.fromhex('ffffff') if safety == 'on' else bytes(3)
+		self.send(2, argument)
+			
+	def send_locations(self, locations):
+		pass
+			
+	def send(self, command, argument, is_signed = False):
+		assert type(argument) in [int, bytes]
+		if isinstance(argument, int):
+			argument = argument.to_bytes(3, 'big', signed = is_signed)
+		message = command.to_bytes + argument
+		self.client.send(message)
+		
 class Receive_Thread (threading.Thread):
 	
-	def __init__(self, socket, master_queue):
+	def __init__(self, socket, master_queue, stop_flag):
 		self.socket = socket
 		self.master_queue = master_queue
+		self.stop_flag = stop_flag
 		
 	def run(self):
 		while True:
+			if self.stop_flag.is_set():
+				break
 			data = self.socket.recv(4)
 			if data:
-				command = hex(data[0])
+				command = bytes(data[0])
 				argument = bytes([data[1], data[2], data[3]])
+				if config.echo:
+					print 'Received command %s with argument %s' % (command,
+																   argument)
 				self.master_queue.put([command, argument])
 				
